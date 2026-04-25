e6cc2649c1ffda4299d47c4fb2146557b43f712a
dongheming <dongheming@bytedance.com>
2026-04-25 13:49:49 +0800

    refactor: extract rename business logic to rename_service

--- src/application/services/mod.rs
+++ src/application/services/mod.rs
@@ -2,9 +2,11 @@
 pub mod delete_service;
 pub mod download_service;
 pub mod list_service;
+pub mod rename_service;
 pub mod sync_executor;
 pub mod sync_service;
 
 pub use delete_service::delete;
 pub use download_service::download;
 pub use list_service::{ListResult, list};
+pub use rename_service::rename;
